eeca0a9253bd7e61f908f8d32399a3cd6a5dfadb
Ethan Lin <ethan28lin@gmail.com>
2025-02-12 15:35:06 -0800

    fixes for scraper unit tests

--- data-collection/tests/test_CourseScraper.py
+++ data-collection/tests/test_CourseScraper.py
@@ -1,60 +1,61 @@
 import unittest
 import pandas as pd
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from src.CourseScraper import CourseScraper
 
 expected_base = 'https://www.scu.edu/bulletin/undergraduate/'
 
 expected_tag_map = {
-        # CAS
-        'Anthropology': 'ANTH',
-        'Art  and  Art  History': 'ARTH',
-        'Biology': 'BIOL',
-        'Chemistry and Biochemistry': 'CHEM',
-        'Child Studies': 'CHST',
-        'Classics': 'CLAS',
-        'Communication': 'COMM',
-        'Economics': 'ECON',
-        'English': 'ENGL',
-        'Environmental  Studies  and  Sciences': 'ENVS',
-        'Ethnic Studies': 'ETHN',
-        'History': 'HIST',
-        'Mathematics and Computer Science': 'MATH/CSCI',
-        'Modern Languages and Literatures': '????',
-        'Music': 'MUSC',
-        'Neuroscience': 'NEUR',
-        'Philosophy': 'PHIL',
-        'Physics': 'PHYS',
-        'Political Science': 'POLI',
-        'Psychology': 'PSYC',
-        'Public  Health  Department': 'PHSC',
-        'Religious Studies': 'TESP',
-        'Sociology': 'SOCI',
-        'Theatre  and  Dance': 'THTR/DANC',
-        'Womens and Gender Studies': 'WGST',
-        'Gender and Sexuality Studies': 'WGST',
-        'Arabic Islamic and Middle Eastern Studies': '????',
-        'Asian Studies': 'ASIA',
-        'Catholic Studies': 'ASCI',
-        'Latin American Studies': '????',
-        'Premodern  Studies': '????',
-
-        # LSB
-        'Management': 'MGMT',
-        'Marketing': 'MKTG',
-        'Information  Systems &  Analytics': 'OMIS',
-        'Accounting': 'ACTG',
-        'Economics': 'ECON',
-        'Finance': 'FNCE',
-
-        # SOE
-        'Applied Mathematics': 'AMTH',
-        'Bioengineering' : 'BIOE',
-        'Civil, Environmental, and Sustainable  Engineering' : 'CENG',
-        'Computer  Science  and  Engineering' : 'CSEN',
-        'Electrical and Computer Engineering' : 'ECEN',
-        'General Engineering' : 'ENGR',
-        'Mechanical  Engineering': 'MECH'
-}
+            'Anthropology': 'ANTH',
+            'Art  and  Art  History': 'ARTH',
+            'Biology': 'BIOL',
+            'Chemistry and Biochemistry': 'CHEM',
+            'Child Studies': 'CHST',
+            'Classics': 'CLAS',
+            'Communication': 'COMM',
+            'Economics': 'ECON',
+            'English': 'ENGL',
+            'Environmental  Studies  and  Sciences': 'ENVS',
+            'Ethnic Studies': 'ETHN',
+            'Gender and Sexuality Studies': 'WGST',
+            'History': 'HIST',
+            'Mathematics': 'MATH',
+            'Computer Science': 'COEN',
+            'Arabic Studies': 'ARAB',
+            'Chinese Studies': 'CHIN',
+            'French Studies': 'FREN',
+            'German Studies': 'GERM',
+            'Italian Studies': 'ITAL',
+            'Japanese Studies': 'JAPN',
+            'Spanish Studies': 'SPAN',
+            'Music': 'MUSC',
+            'Neuroscience': 'NEUR',
+            'Philosophy': 'PHIL',
+            'Physics': 'PHYS',
+            'Political Science': 'POLI',
+            'Psychology': 'PSYC',
+            'Public  Health  Department': 'PHSC',
+            'Scripture and Tradition (SCTR)': 'SCTR',
+            'Theology, Ethics, and Spirituality (TESP)': 'TESP',
+            'Religion and Society (RSOC)': 'RSOC',
+            'Sociology': 'SOCI',
+            'Theatre': 'THTR',
+            'Dance': 'DANC',
+            'Accounting': 'ACTG',
+            'Finance': 'FNCE',
+            'Management': 'MGMT',
+            'Marketing': 'MKTG',
+            'Information  Systems &  Analytics': 'OMIS',
+            'Applied Mathematics': 'AMTH',
+            'Bioengineering': 'BIOE',
+            'Civil, Environmental, and Sustainable  Engineering': 'CENG',
+            'Computer  Science  and  Engineering': 'CSEN',
+            'Electrical and Computer Engineering': 'ECEN',
+            'General Engineering': 'ENGR',
+            'Mechanical  Engineering': 'MECH',
+        }
 
 class TestCourseScraper(unittest.TestCase):
 
@@ -66,14 +67,12 @@ class TestCourseScraper(unittest.TestCase):
         self.assertEqual(self.scraper.tag_map, expected_tag_map)
 
     def test_get_tag_map(self):
-        actual_tag_map = self.scraper.get_tag_map()
+        actual_tag_map = self.scraper.tag_map
         self.assertEqual(actual_tag_map, expected_tag_map)
 
     def test_retrieve_course_df(self):
         actual_df = self.scraper.retrieve_course_df()
         self.assertIsInstance(actual_df, pd.DataFrame)
-
-    
 
 if __name__ == '__main__':
     unittest.main()
